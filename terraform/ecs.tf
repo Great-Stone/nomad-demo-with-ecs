bb0ecdb137367048e135bc92761b463b4db3c888
Great-Stone <hahohh@gmail.com>
2022-09-20 10:25:06 +0900

    job rendered fix

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -12,17 +12,15 @@ resource "aws_ecs_task_definition" "nomad_remote_driver_demo" {
 }
 
 data "template_file" "nomad_ecs_job" {
-  count    = 2
   template = file("${path.module}/../nomad/job-example/ecs/demo-ecs.tpl")
   vars = {
-    task_definition   = "${aws_ecs_task_definition.nomad_remote_driver_demo.family}:${count.index + 1}"
+    task_definition   = split("/", aws_ecs_task_definition.nomad_remote_driver_demo.arn)[1]
     security_group_id = aws_security_group.nomad_demo.id
     subnet_id         = aws_subnet.nomad_demo.id
   }
 }
 
 resource "local_file" "nomad_ecs_job" {
-  count    = 2
-  content  = element(data.template_file.nomad_ecs_job, count.index).rendered
-  filename = "${path.module}/../nomad/job-example/ecs/demo-ecs-demo-${count.index + 1}.hcl"
+  content  = data.template_file.nomad_ecs_job.rendered
+  filename = "${path.module}/../nomad/job-example/ecs/demo-ecs-demo.hcl"
 }
